c57ec70adb352bf97d4d0bb115aae5d94014727a
GustavoFavorito <gustavofavorito.d@gmail.com>
2019-04-08 12:32:23 -0300

    Ao clicar em um item da lista, uma nova Activity é criada e o google maps pesquisa pela coordenada informada

--- app/src/main/java/br/usjt/ciclodevidagpsemapas/DetalhesLocalizacoesActivity.java
+++ app/src/main/java/br/usjt/ciclodevidagpsemapas/DetalhesLocalizacoesActivity.java
@@ -1,16 +1,24 @@
 package br.usjt.ciclodevidagpsemapas;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Locale;
+
 public class DetalhesLocalizacoesActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_localizacoes);
-        Intent origemIntent = getIntent();
 
+        Uri gmmIntentUri = Uri.parse("geo:15.7749,-122.4194");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
